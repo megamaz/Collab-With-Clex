cebcac0a9c85f0c7271b529d6a4c92f9b04cd61b
Code Crash <50129568+CodeCrashStudios@users.noreply.github.com>
2020-02-28 17:57:12 -0500

    Lasers!
    Pew pew (pewdiepie!)

---
+++ Collab WIth clex/Assets/prefabs/Laser/scripts/laser.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class laser : MonoBehaviour
+{
+
+    public Transform point1;
+    public Transform point2;
+
+    public LineRenderer laserLine;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        laserLine.SetPosition(0, point1.position);
+        laserLine.SetPosition(1, point2.position);
+    }
+}
