3ba1a831509377836895cee70d3c6415e1c0b2c1
megamaz <44149188+megamaz@users.noreply.github.com>
2020-02-27 19:22:50 -0800

    Upload 2.27.2020 7:21PM/US West
    Added character controls and expanded the scene.

---
+++ Collab WIth clex/Assets/Scripts/MouseLook.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    
+    public float MouseSensitivity;
+    
+    public Transform playerBody;
+    float xrotation = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+        Looking around script. 
+        Script is not mine idk what im doing
+
+        megamaz
+        */
+        float mouseX = Input.GetAxis("Mouse X") * MouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * MouseSensitivity * Time.deltaTime;
+
+        xrotation -= mouseY; 
+        xrotation = Mathf.Clamp(xrotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xrotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+        
+
+    }
+}
---
+++ Collab WIth clex/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    
+    public CharacterController controller;
+    
+    public float speed = 12f;
+
+    Vector3 velocity;
+    public float gravity = -9.81f;
+    public float JumpHeight = 3f;
+
+    public Transform groundCheck;
+    public float distance;
+    public LayerMask groundmask;
+    public float grounddistance = 0.04f;
+    bool isgruonded; //I realized i made a typo in this variable too late. Oops.
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+        Movement script
+        I again still don't know what I'm doing
+        
+        megamaz
+        */
+        isgruonded = Physics.CheckSphere(groundCheck.position, grounddistance, groundmask);
+
+        if (isgruonded && velocity.y < 0){
+            velocity.y = -2f;
+        }
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+
+        if(Input.GetButtonDown("Jump") && isgruonded ){
+            velocity.y = Mathf.Sqrt(JumpHeight * -2 * gravity);
+        }
+    }
+}
