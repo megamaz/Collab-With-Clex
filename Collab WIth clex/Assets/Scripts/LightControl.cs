986769fcbbdccc2f52d6834d248bc298b5aed3cd
megamaz <44149188+megamaz@users.noreply.github.com>
2020-02-28 21:40:51 -0800

    Added Head Lights
    Yes

---
+++ Collab WIth clex/Assets/Scripts/LightControl.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightControl : MonoBehaviour
+{
+    public bool isOn;
+
+    public Light lightControl;
+    /*
+
+    bool = true / false
+    int = 1, 2, 3
+    float = 0.1, 3.5, 30.4
+    string = "Hello World"
+
+
+    */
+    // Update is called once per frame
+    void Update()
+    {
+        // Light Toggle
+        if(Input.GetKeyDown(KeyCode.F) && isOn){
+            isOn = false;
+        }
+        else if(Input.GetKeyDown(KeyCode.F) && !isOn){
+            isOn = true;
+        }
+
+
+        // Update on / off
+        lightControl.enabled = isOn;
+        
+    }
+}
