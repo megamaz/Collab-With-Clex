31815d34b9d8b0a0188c7cc7fd1b0ceac7e20ff9
megamaz <44149188+megamaz@users.noreply.github.com>
2020-02-28 20:17:51 -0800

    Leveling and Transition
    Added two levels (second one mostly blank)

---
+++ Collab WIth clex/Assets/Scripts/SceneLoad.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoad : MonoBehaviour
+{
+    public bool reset; // Simply reset the level instead of having to put build ID
+
+    public int SceneID; 
+
+    void OnTriggerEnter(){ // Detects if player ran into <object>
+    if (reset){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+    else{
+        SceneManager.LoadScene(SceneID);
+    }
+    }
+}
