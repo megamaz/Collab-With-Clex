9dad8c4f06a2dbed823358fb582710c0b8664c62
Clex-X <61530879+Clex-X@users.noreply.github.com>
2020-03-06 22:04:54 -0600

    Attempt to revise the Drone's movement
    Added a couple variables, changed a few, and reworked movement by adding a new equation.

--- Collab WIth clex/Assets/Scripts/DroneAI.cs
+++ Collab WIth clex/Assets/Scripts/DroneAI.cs
@@ -6,9 +6,11 @@ public class DroneAI : MonoBehaviour
 {
 
     public Transform Player;
-    public float MoveSpeed = 2;
-    public float MaxDist = 40;
-    public float MinDist = 15;
+    public float MoveSpeed = 100;
+	public float Range = 100;
+    public float MaxDist = 10;
+    public float MinDist = 5;
+	public float Drift = 5;
     public float Damping = 1f;
     private bool useGrivaty;
     bool isDead = false;
@@ -23,19 +25,22 @@ public class DroneAI : MonoBehaviour
             transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * Damping);
         
         // Enemy will follow if distance is within these parameters(MinDist and MaxDist)
-        if (Vector3.Distance(transform.position, Player.position) >= MinDist)
+        if (Vector3.Distance(transform.position, Player.position) <= Range)
             {
-                transform.position += transform.forward * MoveSpeed * Time.deltaTime;
 
-                if (Vector3.Distance(transform.position, Player.position) <= MaxDist)
-                {
-                    transform.position += transform.forward * MoveSpeed * Time.deltaTime;
-                }
-            }
+				//If all fails transform.position += transform.forward * MoveSpeed * Time.deltaTime
+
+				transform.position += (transform.forward * Time.deltaTime * ((MoveSpeed * ((Vector3.Distance(transform.position, Player.position)) - MinDist) / (Mathf.Abs((Vector3.Distance(transform.position, Player.position)) - MinDist) + (1 / Drift))) + (MoveSpeed * ((Vector3.Distance(transform.position, Player.position)) - MaxDist) / (Mathf.Abs((Vector3.Distance(transform.position, Player.position)) - MaxDist) + (1 / Drift)))) / 2);
+				//transform.position += transform.forward * Time.deltaTime * MoveSpeed;
+
+			}
         }
         else
         {
             isDead = true;
         }
-    }
+		//Debug.Log((((MoveSpeed * ((Vector3.Distance(transform.position, Player.position)) - MinDist) / (Mathf.Abs((Vector3.Distance(transform.position, Player.position)) - MinDist) + (1 / Drift))) + (MoveSpeed * ((Vector3.Distance(transform.position, Player.position)) - MaxDist) / (Mathf.Abs((Vector3.Distance(transform.position, Player.position)) - MaxDist) + (1 / Drift)))) / 2));
+		//Debug.Log(Vector3.Distance(transform.position, Player.position));
+		Debug.Log(transform.position += (transform.forward * Time.deltaTime * ((MoveSpeed * ((Vector3.Distance(transform.position, Player.position)) - MinDist) / (Mathf.Abs((Vector3.Distance(transform.position, Player.position)) - MinDist) + (1 / Drift))) + (MoveSpeed * ((Vector3.Distance(transform.position, Player.position)) - MaxDist) / (Mathf.Abs((Vector3.Distance(transform.position, Player.position)) - MaxDist) + (1 / Drift)))) / 2));
+	}
 }
