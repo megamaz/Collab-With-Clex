9fce3e5c04bd87282c998dd5f69378e52abc196d
BashirHilaly <54788744+BashirHilaly@users.noreply.github.com>
2020-04-12 02:39:56 -0400

    Player Racast
    Setup raycasts that shoot forward. Will make them shoot to the sides and use an algorithm for the ability for the player to wall run.

---
+++ Collab WIth clex/Assets/WallRunning.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallRunning : MonoBehaviour
+{
+
+    public float targetDistance;
+
+    void Update()
+    {
+        RaycastHit theHit;
+        {
+            if (Physics.Raycast (transform.position, transform.TransformDirection (Vector3.forward), out theHit))
+            {
+                targetDistance = theHit.distance;
+            }
+        }
+    }
+}
